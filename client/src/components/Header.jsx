e1d2b5342c877f109e0a445781c2645cc3357e90
Lovenish <heelme1181@gmail.com>
2024-08-27 14:49:55 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,12 +5,12 @@ export default function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-       <Link to="/">
-       <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Lovenish</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
-       </Link>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Lovenish</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
@@ -23,13 +23,17 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-          <li className="hidden sm:inline text-slate-700 hover:underline">HOME</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              HOME
+            </li>
           </Link>
           <Link to="/about">
-          <li className="hidden sm:inline text-slate-700 hover:underline">ABOUT</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              ABOUT
+            </li>
           </Link>
           <Link to="/sign-in">
-          <li className=" text-slate-700 hover:underline">SIGN IN</li>
+            <li className=" text-slate-700 hover:underline">SIGN IN</li>
           </Link>
         </ul>
       </div>
