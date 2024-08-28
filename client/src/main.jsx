0a6e742c329d7bbfe4d6f60a7a793533c1aa4484
Lovenish <heelme1181@gmail.com>
2024-08-28 16:40:18 +0530

    added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { persiststor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
-  
-    <Provider store={store} >
-    <App />
-    </Provider>
- 
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persiststor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
