b789e49044e7bcb7cf9050a4f1d1c34fdda90e04
Lovenish <heelme1181@gmail.com>
2024-09-02 13:19:53 +0530

    created search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,19 @@
-import express from 'express';
-import { createListing, getListing ,updateListing,  deleteListing} from '../controllers/listing.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  createListing,
+  getListing,
+  updateListing,
+  deleteListing,
+  getListings,
+} from "../controllers/listing.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/create', verifyToken ,  createListing)
-router.delete('/delete/:id', verifyToken ,  deleteListing)
-router.post('/update/:id', verifyToken ,  updateListing)
-router.get('/get/:id', getListing)
+router.post("/create", verifyToken, createListing);
+router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/get/:id", getListing);
+router.get("/get", getListings);
 
-export default router
+export default router;
