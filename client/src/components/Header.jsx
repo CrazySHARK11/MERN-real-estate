6b04ea746d561e98ccf29188a23a3c5964607053
Lovenish <heelme1181@gmail.com>
2025-02-03 19:24:15 +0530

    feb 25

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,32 +7,36 @@ export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const urlParams = new URLSearchParams(window.location.search)
-    urlParams.set('searchTerm', searchTerm);
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm')
+    const searchTermFromUrl = urlParams.get("searchTerm");
 
-    if(searchTermFromUrl){
-     setSearchTerm(searchTermFromUrl)
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
     }
- },[location.search])
+  }, [location.search]);
 
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-slate-900 shadow-md">
+      
+      <div class="w-full bg-yellow-400 py-2 z-50 overflow-hidden">
+        <div class="whitespace-nowrap animate-marquee text-red-600 font-bold text-lg">
+          🚧 This project is under development. Stay tuned for updates! 🚧
+        </div>
+      </div>
+
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Lovenish</span>
-            <span className="text-slate-700">Estate</span>
-          </h1>
+          <img src="./logo.png" width={"90px"} alt="" />
         </Link>
         <form
           onSubmit={handleSubmit}
@@ -53,12 +57,12 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-200 hover:underline">
               HOME
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-200 hover:underline">
               ABOUT
             </li>
           </Link>
@@ -70,7 +74,7 @@ export default function Header() {
                 className="rounded-full h-7 w-7 object-cover"
               />
             ) : (
-              <li className=" text-slate-700 hover:underline">SIGN IN</li>
+              <li className=" text-slate-200 hover:underline">SIGN IN</li>
             )}
           </Link>
         </ul>
